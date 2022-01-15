b2287b0265c2a464ba1a5ea8ba67c4b0881071ad
Luis Garcia <lalejandro.1988@gmail.com>
2022-01-15 10:43:45 -0800

    Alert dialog y Cupertino Alert dialog

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -4,22 +4,39 @@ class AppTheme {
   static const Color primary = Colors.indigo;
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-      // Color primario
-      primaryColor: Colors.indigo,
+    // Color primario
+    primaryColor: Colors.indigo,
 
-      // AppBar Theme
-      appBarTheme: const AppBarTheme(
-        color: primary,
+    // AppBar Theme
+    appBarTheme: const AppBarTheme(
+      color: primary,
+      elevation: 0
+    ),
+
+    // TextButton Theme
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(primary: primary)
+    ),
+
+    // FloatingActionButtons
+    floatingActionButtonTheme: const FloatingActionButtonThemeData (
+      backgroundColor: primary,
+      elevation: 5
+    ),
+
+    // ElevatedButton
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        primary: Colors.indigo,
+        shape: const StadiumBorder(),
         elevation: 0
-      ),
-
-      // TextButton Theme
-      textButtonTheme: TextButtonThemeData(
-        style: TextButton.styleFrom(primary: primary)
       )
+    )
+
   );
 
-    static final ThemeData darkTheme = ThemeData.light().copyWith(
+
+  static final ThemeData darkTheme = ThemeData.light().copyWith(
       // Color primario
       primaryColor: Colors.indigo,
 
